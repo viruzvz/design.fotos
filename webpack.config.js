bf50984fc0e065ed15797fab1956835f525783b7
fabianonunes <fabiano@nunes.me>
2017-09-06 17:06:48 -0300

    remover codigo não utilizado

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,6 @@
 const mosaaaico = require('mosaaaico2')
 
-const ENV = process.env.NODE_ENV || 'development'
-const port = Number(process.env.PORT) || 8000
-const isProduction = ENV === 'production'
-
 const config = mosaaaico({
-  output: {
-    publicPath: isProduction
-      ? '/hpsenado/++resource++senado.hpsenado/'
-      : `http://localhost:${port}/`
-  },
-
-  module: {
-    rules: [{
-      test: /\.yaml$/,
-      loader: 'json-loader!yaml-loader'
-    }]
-  },
-
   externals: {
     jquery: 'jQuery'
   }
