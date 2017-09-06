94bdf02ad0aad93ee5bb32718e5fdcfdb18a78fe
fabianonunes <fabiano@nunes.me>
2017-09-06 17:50:50 -0300

    lint e adição de deps

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,23 @@
 var $ = require('jquery')
 var flatpickr = require('flatpickr')
-var ekkolightbox = require('ekko-lightbox')
-const Portuguese = require("flatpickr/dist/l10n/pt.js").pt;
-flatpickr("#tirada", {
-  dateFormat: 'd/m/Y',
-  locale: Portuguese,
-  mode: 'range',
-}
-);
 
-$(document).on('click', '[data-toggle="lightbox"]', function(event) {
-    event.preventDefault();
-    $(this).ekkoLightbox();
-});
+require('ekko-lightbox')
+require('justifiedGallery/dist/js/jquery.justifiedGallery')
+require('bootstrap')
 
-$( document ).ready(function() {
-$("#mygallery").justifiedGallery();
-});
+const Portuguese = require('flatpickr/dist/l10n/pt.js').pt
+
+$(document).on('click', '[data-toggle="lightbox"]', function (event) {
+  event.preventDefault()
+  $(this).ekkoLightbox()
+})
+
+$(document).ready(function () {
+  $('#mygallery').justifiedGallery()
+
+  flatpickr('#tirada', {
+    dateFormat: 'd/m/Y',
+    locale: Portuguese,
+    mode: 'range'
+  })
+})
