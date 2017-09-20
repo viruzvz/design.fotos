8718738d3a41a0fe9b325db392c2142412d5d05f
Carlos Eduardo Gomes Neri <cegn@senado.leg.br>
2017-09-20 17:33:09 -0300

    carousel-galeria e ajustes

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,7 @@ var flatpickr = require('flatpickr')
 require('ekko-lightbox')
 require('justifiedGallery/dist/js/jquery.justifiedGallery')
 require('bootstrap')
-
+require('simplebar/dist/simplebar.js')
 
 const Portuguese = require('flatpickr/dist/l10n/pt.js').pt
 
@@ -12,7 +12,6 @@ $(document).on('click', '[data-toggle="lightbox"]', function (event) {
   event.preventDefault()
   $(this).ekkoLightbox()
 })
-
 $(document).ready(function () {
   $('#mygallery').justifiedGallery({
     margins: 3
@@ -22,5 +21,4 @@ $(document).ready(function () {
     locale: Portuguese,
     mode: 'range'
   })
-  $('.mCustomScrollbar').mCustomScrollbar({axis: 'x'})
 })
